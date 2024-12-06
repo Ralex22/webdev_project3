e93c3bfebe853f9e3a75b0dfa4905d30cabd7469
Ralex22 <ralexander2@hawk.iit.edu>
2024-12-06 09:54:38 -0600

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
  function initMap(){
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 42.3601, lng: 71.0589 },
+    center: {lat: 42.3601° N, lng: 71.0589° W },
     zoom: 8,
     mapTypeControl: true,
     mapTypeControlOptions: {
@@ -11,7 +11,7 @@
   });
 
   var marker = new google.maps.Marker({
-    position: {lat: 42.3601, lng: 71.0589},
+    position: {lat: 42.3601° N, lng: 71.0589° W},
     map: map,
     title: 'Your Location'
   });
@@ -31,7 +31,7 @@
     fillColor: "#FF0000",
     fillOpacity: 0.35,
     map: map,
-    center: { lat: 42.3601, lng: 71.0589 },
+    center: { lat: 42.3601° N, lng: 71.0589° W },
     radius: 4000
   });
     
